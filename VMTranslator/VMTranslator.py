40378202764cb98aec0ff791f88915554c0dc940
azizul-hakim697 <82722111+azizul-hakim697@users.noreply.github.com>
2025-10-18 05:06:20 +0000

    hello

--- VMTranslator/VMTranslator.py
+++ VMTranslator/VMTranslator.py
@@ -106,46 +106,52 @@ class VMTranslator:
 
     def vm_return():
         return ""
+if __name__ == "__main__":
+    import sys
+    if(len(sys.argv) > 1):
+        with open(sys.argv[1], "r") as a_file:
+            for line in a_file:
+                tokens = line.strip().lower().split()
+                if(len(tokens)==1):
+                    if(tokens[0]=='add'):
+                        print(VMTranslator.vm_add())
+                    elif(tokens[0]=='sub'):
+                        print(VMTranslator.vm_sub())
+                    elif(tokens[0]=='neg'):
+                        print(VMTranslator.vm_neg())
+                    elif(tokens[0]=='eq'):
+                        print(VMTranslator.vm_eq())
+                    elif(tokens[0]=='gt'):
+                        print(VMTranslator.vm_gt())
+                    elif(tokens[0]=='lt'):
+                        print(VMTranslator.vm_lt())
+                    elif(tokens[0]=='and'):
+                        print(VMTranslator.vm_and())
+                    elif(tokens[0]=='or'):
+                        print(VMTranslator.vm_or())
+                    elif(tokens[0]=='not'):
+                        print(VMTranslator.vm_not())
+                    elif(tokens[0]=='return'):
+                        print(VMTranslator.vm_return())
+                elif(len(tokens)==2):
+                    if(tokens[0]=='label'):
+                        print(VMTranslator.vm_label(tokens[1]))
+                    elif(tokens[0]=='goto'):
+                        print(VMTranslator.vm_goto(tokens[1]))
+                    elif(tokens[0]=='if-goto'):
+                        print(VMTranslator.vm_if(tokens[1]))
+                elif(len(tokens)==3):
+                    if(tokens[0]=='push'):
+                        print(VMTranslator.vm_push(tokens[1],int(tokens[2])))
+                    elif(tokens[0]=='pop'):
+                        print(VMTranslator.vm_pop(tokens[1],int(tokens[2])))
+                    elif(tokens[0]=='function'):
+                        print(VMTranslator.vm_function(tokens[1],int(tokens[2])))
+                    elif(tokens[0]=='call'):
+                        print(VMTranslator.vm_call(tokens[1],int(tokens[2])))
 
-import sys
-if(len(sys.argv) > 1):
-    with open(sys.argv[1], "r") as a_file:
-        for line in a_file:
-            tokens = line.strip().lower().split()
-            if(len(tokens)==1):
-                if(tokens[0]=='add'):
-                    print(VMTranslator.vm_add())
-                elif(tokens[0]=='sub'):
-                    print(VMTranslator.vm_sub())
-                elif(tokens[0]=='neg'):
-                    print(VMTranslator.vm_neg())
-                elif(tokens[0]=='eq'):
-                    print(VMTranslator.vm_eq())
-                elif(tokens[0]=='gt'):
-                    print(VMTranslator.vm_gt())
-                elif(tokens[0]=='lt'):
-                    print(VMTranslator.vm_lt())
-                elif(tokens[0]=='and'):
-                    print(VMTranslator.vm_and())
-                elif(tokens[0]=='or'):
-                    print(VMTranslator.vm_or())
-                elif(tokens[0]=='not'):
-                    print(VMTranslator.vm_not())
-                elif(tokens[0]=='return'):
-                    print(VMTranslator.vm_return())
-            elif(len(tokens)==2):
-                if(tokens[0]=='label'):
-                    print(VMTranslator.vm_label(tokens[1]))
-                elif(tokens[0]=='goto'):
-                    print(VMTranslator.vm_goto(tokens[1]))
-                elif(tokens[0]=='if-goto'):
-                    print(VMTranslator.vm_if(tokens[1]))
-            elif(len(tokens)==3):
-                if(tokens[0]=='push'):
-                    print(VMTranslator.vm_push(tokens[1],int(tokens[2])))
-                elif(tokens[0]=='pop'):
-                    print(VMTranslator.vm_pop(tokens[1],int(tokens[2])))
-                elif(tokens[0]=='function'):
-                    print(VMTranslator.vm_function(tokens[1],int(tokens[2])))
-                elif(tokens[0]=='call'):
-                    print(VMTranslator.vm_call(tokens[1],int(tokens[2])))
+        
+
+
+
+
